f20c8ee4e20cb30096f8603b940f019530083a73
annguyen0606 <anuet59@gmail.com>
2020-09-24 23:44:02 +0700

    init trang admin

---
+++ QuanLyNoiBoAdminApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyNoiBoAdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
